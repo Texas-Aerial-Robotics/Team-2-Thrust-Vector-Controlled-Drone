8a84a84b27568cfc8295268dd56d6c391e7878cb
Joshua Caratao <53455636+JoshCaratao@users.noreply.github.com>
2024-09-02 17:51:56 -0500

    Added Matlab Files

---
+++ TVC-Drone-Simulation/ModelVariables.m
@@ -0,0 +1,24 @@
+
+
+%Vehicle Mechanical Properties
+Mass = 1; %kg
+Thrust_Moment_Arm = 0.1; %m\
+
+%Gravity
+Gravity_Vector_Inertial = [0,0,Mass*9.81]; %Positive because intertial frame uses Z as positive down
+
+%Initial Values for Simulation
+Initial_Pos_Inertial_Axes = [0,0,0]; %m
+Initial_Vel_Body_Axes = [0,0,0]; %m/s
+Initial_Euler_Orientation = [0,0,0]; %[Roll,Pitch,Yaw]
+Initial_Body_Rotation_Rates = [0,0,0]; %[p,q,r] (rad/s)
+Initial_Mass = Mass; %kg
+Inertia_Tensor = [
+    0.25, 0, 0;
+    0, 0.25, 0;
+    0, 0, 0.25
+]; %[3x3] matrix that includes principal and products of inertia (kgm^2)
+
+
+%Reference Signal
+z_Reference = 30;
